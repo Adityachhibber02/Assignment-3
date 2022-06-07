6581d8d0a8f22df6105a609e6cdccabf9b3c6fbf
Aditya Chhibber <102915313+Adityachhibber02@users.noreply.github.com>
2022-06-07 18:58:35 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,26 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:\Users\Aditya\Desktop\Assignment 3.py ===============
+Enter your number48
+110000
+Enter first number: 74
+Enter sign(+, -, *, / or q for quit): +
+Next number: 95
+Enter next sign(+, -, *, / or q for quit): *
+Next number: 56
+Enter next sign(+, -, *, / or q for quit): q
+9464.0
+35
+45.0
+1256.6370614359173
+3.1622776601683795
+0.5
+0 1 2 3 4 
+3 4 5 6 7 8 9 
+4 7 10 
+15 13 11 9 7 
+Enter number of Hydrogen Atoms: 4
+Enter number of Carbon Atoms: 6
+Enter number of Oxygen Atoms: 3
+Molecular weight of compound is: 124.09416 grams/mole
